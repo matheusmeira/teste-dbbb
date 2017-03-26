55c926bd128ac76b81093072554fc324094d3f8f
Matheus <meira.matheus.rob@gmail.com>
2017-03-26 12:40:57 -0300

    Cards

--- zup/js/home.controller.js
+++ zup/js/home.controller.js
@@ -17,14 +17,23 @@ app.controller('homeCtrl', function($scope, $rootScope, $sce) {
     switch (index) {
       case 1:
         $scope.cardShotBigWithIcons = false;
+        $scope.cardShotLittleOffIcons = false;
+        $scope.cardShotOffIcons = false;
         break;
       case 2:
         $scope.cardShotBigWithIcons = true;
+        $scope.cardShotLittleOffIcons = false;
+        $scope.cardShotOffIcons = false;
         break;
       case 3:
-        $scope.cardShotOffIcons = true;
+        $scope.cardShotLittleOffIcons = true;
+        $scope.cardShotBigWithIcons = false;
+        $scope.cardShotOffIcons = false;
         break;
       case 4:
+        $scope.cardShotOffIcons = true;
+        $scope.cardShotLittleOffIcons = false;
+        $scope.cardShotBigWithIcons = false;
         break;
       default:
 
@@ -32,8 +41,14 @@ app.controller('homeCtrl', function($scope, $rootScope, $sce) {
   }
 
   $scope.deliberatelyTrustDangerousSnippet = function(snippet) {
-    
+
     return $sce.trustAsHtml(snippet);
   };
+
+  $('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
 
 });
